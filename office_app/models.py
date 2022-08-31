aa79138a150345c2eec33a1a8d171bfaa0abe49f
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-08-31 23:08:07 +0530

    added field lookup comments for future ref

--- office_app/models.py
+++ office_app/models.py
@@ -15,3 +15,22 @@ class patient(models.Model):
 # patient.objects.bulk_create("""a list of patients as in: patient_list=[patient(fname='peru',lname='inshal',age=30),patient(fname='peru',lname='inshal',age=30)]...""") // a lot of items in one go
 
 # patient.objects.all()  //getting all the data available in tht db
+
+# patient.objects.get(pk=5) //getting data from object with primary key/ pk=5
+
+
+#------------- Filter samples-----------------
+
+# patient.objects.filter(lname='smith') -- can get all the patients with lastname smith
+
+# patient.objects.filter(lname='smith').filter(age=40)
+
+
+#-------------> Field LookUps <-------------------
+
+# To use more boolean operators such as <,>,<=,>= etc
+
+# patient.objects.filter(name__startswith="s")  --> here two underscores "__" after field name is really important before lookup function.
+#                                 |_________ here startswith is a function/fieldlookup thing to chk the corresponding
+
+                                        # |____ There are a lot of those, check documntation -- " field-lookups "
