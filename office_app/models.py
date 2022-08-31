e47d9e904235ba99926bf56370b4fd5da8b92990
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2022-08-31 22:46:19 +0530

    some model basics in shell, creation and addition of objects to the db

--- office_app/models.py
+++ office_app/models.py
@@ -5,3 +5,11 @@ class patient(models.Model):
     lname = models.CharField(max_length=30)
     age = models.IntegerField()
     
+    def __str__(self):
+        return f"{self.fname} {self.lname} of age {self.age}"
+    
+# patient.objects.create(fname='peru',lname='inshal',age=30) //for all in one go
+
+# patient1 = patient(fname='peru',lname='inshal',age=30) &&&& patient1.save()
+
+# patient.objects.bulk_create("""a list of patients as in: patient_list=[patient(fname='peru',lname='inshal',age=30),patient(fname='peru',lname='inshal',age=30)]...""") // a lot of items in one go
